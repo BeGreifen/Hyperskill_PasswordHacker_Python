9e59dd2c20702ce9c9527f76ac3a398a8e554999
gnubrew <b.greifen@gmail.com>
2023-08-13 13:08:24 +0200

    +) establish connection and send message

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,12 +1,18 @@
 import argparse
+import socket
+
+
 # write your code here
 
 
 def set_args():
-    parser = argparse.ArgumentParser(description="Getting commandline inputs to start Hacking Password script.")
-    parser.add_argument("ip_address", type=str)
-    parser.add_argument("port", type=int)
-    parser.add_argument("message", type=str)
+    parser = argparse.ArgumentParser(description="Commandline inputs to start Hacking Password script.")
+    parser.add_argument("ip_address", type=str,
+                        help="like localhost or 127.0.0.1")
+    parser.add_argument("port", type=int,
+                        help="like 8080")
+    parser.add_argument("message", type=str,
+                        help="string that you want to send, like this is a test")
     return parser
 
 
@@ -16,9 +22,27 @@ def get_args() -> argparse:
     return parser
 
 
+def send_message_and_get_response(hostname: str, port: int, message: str) -> socket:
+    with socket.socket() as client_socket:
+        address = (hostname, port)
+        client_socket.connect(address)
+
+        message = message.encode()
+        client_socket.send(message)
+
+        response = client_socket.recv(port)
+        response = response.decode()
+        return response
+
+
 def main():
     parser = get_args()
-    print(parser.parse_args())
+    args = parser.parse_args()
+    ans = send_message_and_get_response(args.ip_address,
+                                        args.port,
+                                        args.message)
+    print(ans)
+
 
 if __name__ == "__main__":
     main()
