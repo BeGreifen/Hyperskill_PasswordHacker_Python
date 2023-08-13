24aea12d18690d3b267aca4f8a8890792156d891
gnubrew <b.greifen@gmail.com>
2023-08-13 21:19:10 +0200

    o) change files +) solution popping the losers

---
+++ Topics/Collections module/Popping the losers/main.py
@@ -0,0 +1,9 @@
+import json
+
+# the following line reads a dict from the input and converts it into an OrderedDict, do not modify it, please
+firms = OrderedDict(json.loads(input()))
+
+# your code here
+firms.popitem(last=True)
+firms.popitem(last=True)
+print(firms)
